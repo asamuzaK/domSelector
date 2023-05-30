22337bca4307cf5391ae47871afbc352e0bdc76a
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-05-30 09:34:18 +0900

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -2140,6 +2140,48 @@ describe('exported api', () => {
         ], 'result');
       });
     });
+
+    it('should not match', () => {
+      const res = querySelectorAll(':not(*)', document);
+      assert.deepEqual(res, [], 'result');
+    });
+
+    it('should get matched node(s)', () => {
+      const domStr = `<div id="root">
+        <div id="attr-whitespace">
+          <div id="attr-whitespace-div1" class="foo div1 bar"></div>
+          <div id="attr-whitespace-div2" class=""></div>
+          <div id="attr-whitespace-div3" class="foo div3 bar"></div>
+
+          <div id="attr-whitespace-div4" data-attr-whitespace="foo &#xE9; bar"></div>
+          <div id="attr-whitespace-div5" data-attr-whitespace_foo="&#xE9; foo"></div>
+
+          <a id="attr-whitespace-a1" rel="next bookmark"></a>
+          <a id="attr-whitespace-a2" rel="tag nofollow"></a>
+          <a id="attr-whitespace-a3" rel="tag bookmark"></a>
+          <a id="attr-whitespace-a4" rel="book mark"></a> <!-- Intentional space in "book mark" -->
+          <a id="attr-whitespace-a5" rel="nofollow"></a>
+          <a id="attr-whitespace-a6" rev="bookmark nofollow"></a>
+          <a id="attr-whitespace-a7" rel="prev next tag alternate nofollow author help icon noreferrer prefetch search stylesheet tag"></a>
+
+          <p id="attr-whitespace-p1" title="Chinese 中文 characters"></p>
+        </div>
+      </div>`;
+      document.body.innerHTML = domStr;
+      const res = querySelectorAll("#attr-whitespace a[rel~='bookmark'], #attr-whitespace a[rel~='nofollow']", document);
+      const ids = [
+        'attr-whitespace-a3',
+        'attr-whitespace-a1',
+        'attr-whitespace-a2',
+        'attr-whitespace-a5',
+        'attr-whitespace-a7'
+      ];
+      assert.strictEqual(res.length, 5, 'length');
+      for (const i of ids) {
+        const elm = document.getElementById(i);
+        assert.isTrue(res.includes(elm), 'includes');
+      }
+    });
   });
 });
 
