4ffda807e2bd887b8d79799bc698afc8ab6f44b0
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-06-09 22:04:13 +0900

    Update types

--- types/js/matcher.d.ts
+++ types/js/matcher.d.ts
@@ -1,10 +1,16 @@
 export class Matcher {
-    constructor(selector: string, refPoint: object, opt?: {
-        warn?: object;
+    constructor(selector: string, node: object, opt?: {
+        warn?: boolean;
     });
-    _match(ast: object, node: object): Array<object | undefined>;
-    _getMatchedNodes(branch?: Array<object>, node?: object): Array<object | undefined>;
-    _find(ast: object, node: object): Array<object | undefined>;
+    _onError(e: Error): void;
+    _getRoot(node: object): object;
+    _prepare(): any[];
+    _matchSelector(ast: object, node: object): object;
+    _findNodes(twig: object): object;
+    _collectNodes(range: string): any[];
+    _matchNodes(): object;
+    _find(range: string): object;
+    _sortNodes(nodes: object, range: string): object;
     matches(): boolean;
     closest(): object | null;
     querySelector(): object | null;
@@ -16,26 +22,27 @@ export function collectNthChild(anb?: {
     b: number;
     reverse?: boolean;
     selector?: string;
-}, node?: object): Array<object | undefined>;
+}, node?: object): object;
 export function collectNthOfType(anb?: {
     a: number;
     b: number;
     reverse?: boolean;
-}, node?: object): Array<object | undefined>;
+}, node?: object): object;
 export function createSelectorForNode(node?: object): string | null;
-export function groupASTLeaves(branch?: Array<object>): Array<object>;
-export function isAttached(node?: object): boolean;
+export function factorial(n: number): number;
 export function isContentEditable(node?: object): boolean;
+export function isDescendant(node?: object, root?: object): boolean;
 export function isNamespaceDeclared(ns?: string, node?: object): boolean;
-export function matchAnPlusB(nthName: string, ast?: object, node?: object): Array<object | undefined>;
+export function matchAnPlusB(nthName: string, ast?: object, node?: object): object;
 export function matchAttributeSelector(ast?: object, node?: object): object | null;
 export function matchClassSelector(ast?: object, node?: object): object | null;
-export function matchCombinator(combo?: object, prevNodes?: Array<object>, nextNodes?: Array<object>): Array<object | undefined>;
+export function matchCombinator(combo?: object, prevNodes?: object, nextNodes?: object): object;
 export function matchDirectionPseudoClass(ast?: object, node?: object): object | null;
 export function matchIDSelector(ast?: object, node?: object): object | null;
 export function matchLanguagePseudoClass(ast?: object, node?: object): object | null;
 export function matchLogicalPseudoFunc(ast?: object, node?: object, refPoint?: object): object | null;
-export function matchPseudoClassSelector(ast?: object, node?: object, refPoint?: object): Array<object | undefined>;
+export function matchPseudoClassSelector(ast?: object, node?: object, refPoint?: object): object;
 export function matchPseudoElementSelector(ast?: object, node?: object): void;
 export function matchTypeSelector(ast?: object, node?: object): object | null;
+export function parseASTName(name: string, node?: object): object;
 export function unescapeSelector(selector?: string): string | null;
