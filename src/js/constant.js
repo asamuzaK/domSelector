10323f7509644ebf58def6f6b2b2eec6e7aca01f
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-06-09 22:01:25 +0900

    Update constant.js

--- src/js/constant.js
+++ src/js/constant.js
@@ -3,19 +3,34 @@
  */
 'use strict';
 
+const AN_PLUS_B = 'AnPlusB';
+const ATTRIBUTE_SELECTOR = 'AttributeSelector';
+const CLASS_SELECTOR = 'ClassSelector';
+const COMBINATOR = 'Combinator';
+const ID_SELECTOR = 'IdSelector';
+const IDENTIFIER = 'Identifier';
+const NTH = 'Nth';
+const PSEUDO_CLASS_SELECTOR = 'PseudoClassSelector';
+const PSEUDO_ELEMENT_SELECTOR = 'PseudoElementSelector';
+const RAW = 'Raw';
+const SELECTOR = 'Selector';
+const SELECTOR_LIST = 'SelectorList';
+const STRING = 'String';
+const TYPE_SELECTOR = 'TypeSelector';
+
 module.exports = {
-  AN_PLUS_B: 'AnPlusB',
-  ATTRIBUTE_SELECTOR: 'AttributeSelector',
-  CLASS_SELECTOR: 'ClassSelector',
-  COMBINATOR: 'Combinator',
-  ID_SELECTOR: 'IdSelector',
-  IDENTIFIER: 'Identifier',
-  NTH: 'Nth',
-  PSEUDO_CLASS_SELECTOR: 'PseudoClassSelector',
-  PSEUDO_ELEMENT_SELECTOR: 'PseudoElementSelector',
-  RAW: 'Raw',
-  SELECTOR: 'Selector',
-  SELECTOR_LIST: 'SelectorList',
-  STRING: 'String',
-  TYPE_SELECTOR: 'TypeSelector'
+  AN_PLUS_B,
+  ATTRIBUTE_SELECTOR,
+  CLASS_SELECTOR,
+  COMBINATOR,
+  ID_SELECTOR,
+  IDENTIFIER,
+  NTH,
+  PSEUDO_CLASS_SELECTOR,
+  PSEUDO_ELEMENT_SELECTOR,
+  RAW,
+  SELECTOR,
+  SELECTOR_LIST,
+  STRING,
+  TYPE_SELECTOR
 };
