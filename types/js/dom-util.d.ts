8e7eccb98ea941b0e738885847d2b5af361f8271
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-12-13 09:36:40 +0900

    Update dom-util.d.ts

--- types/js/dom-util.d.ts
+++ types/js/dom-util.d.ts
@@ -3,7 +3,6 @@ export function getSlottedTextContent(node?: object): string | null;
 export function getDirectionality(node?: object): string | null;
 export function isContentEditable(node?: object): boolean;
 export function isNamespaceDeclared(ns?: string, node?: object): boolean;
-export function isDescendant(node?: object, base?: object): boolean;
 export function isInclusive(nodeA?: object, nodeB?: object): boolean;
 export function isPreceding(nodeA?: object, nodeB?: object): boolean;
 export function selectorToNodeProps(selector: string, node?: object): object;
