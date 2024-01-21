b7c81e828c1b5e4ff45334ac8dcbe5e0a10f2755
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-01-21 20:53:02 +0900

    Update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,19 @@
+export { Finder };
+export let finder: Finder;
 export function matches(selector: string, node: object, opt?: {
+    noexcept?: boolean;
     warn?: boolean;
 }): boolean;
 export function closest(selector: string, node: object, opt?: {
+    noexcept?: boolean;
     warn?: boolean;
 }): object | null;
 export function querySelector(selector: string, node: object, opt?: {
+    noexcept?: boolean;
     warn?: boolean;
 }): object | null;
 export function querySelectorAll(selector: string, node: object, opt?: {
+    noexcept?: boolean;
     warn?: boolean;
 }): Array<object | undefined>;
+import { Finder } from './js/finder.js';
