ea124f35a82f5aebd2d68c182e4f9fe3187f88ba
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-02-02 16:01:50 +0900

    v3.0.4

--- types/js/matcher.d.ts
+++ types/js/matcher.d.ts
@@ -1,6 +1,4 @@
 export function _matchAttributeSelector(ast: object, node: object): object | null;
-export function _matchClassSelector(ast: object, node: object): object | null;
-export function _matchIDSelector(ast: object, node: object): object | null;
 export function _matchTypeSelector(ast: object, node: object, opt?: {
     forgive?: boolean;
 }): object | null;
