8137582559e6eeb24970ed8c91eea29d98fafb91
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-02-21 19:42:51 +0900

    Update regexp

--- test/parser.test.js
+++ test/parser.test.js
@@ -6444,26 +6444,6 @@ describe('filter selector (for nwsapi)', () => {
   });
 
   it('should get false', () => {
-    const res = func('[foo="bar baz\']');
-    assert.isFalse(res, 'result');
-  });
-
-  it('should get false', () => {
-    const res = func('[foo=\'bar baz"]');
-    assert.isFalse(res, 'result');
-  });
-
-  it('should get false', () => {
-    const res = func('[foo="bar baz\' i]');
-    assert.isFalse(res, 'result');
-  });
-
-  it('should get false', () => {
-    const res = func('[foo=\'bar baz" i]');
-    assert.isFalse(res, 'result');
-  });
-
-  it('should get false', () => {
     const res = func('[foo bar baz i]');
     assert.isFalse(res, 'result');
   });
