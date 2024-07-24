9df94d35f849167d0182abe76e5d0d8217d41348
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-07-24 21:31:27 +0900

    v5.0.1

--- types/js/nwsapi.d.ts
+++ types/js/nwsapi.d.ts
@@ -1,2 +1,2 @@
-export function initNwsapi(document: object): object;
+export function initNwsapi(window: object, document: object): object;
 export function filterSelector(selector: string, opt?: object): boolean;
