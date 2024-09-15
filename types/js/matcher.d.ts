6c07b7df3b110ef2003e2725149efb9ec955f598
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-09-15 14:58:02 +0900

    Update types

--- types/js/matcher.d.ts
+++ types/js/matcher.d.ts
@@ -1,11 +1,10 @@
-export class Matcher {
-    matchPseudoElementSelector(astName: string, opt?: {
-        forgive?: boolean;
-        warn?: boolean;
-    }): void;
-    private _matchAttributeSelector;
-    private _matchTypeSelector;
-    private _matchDirectionPseudoClass;
-    private _matchLanguagePseudoClass;
-    matchSelector(ast: object, node: object, opt?: object, validated?: boolean): object | null;
-}
+export function matchPseudoElementSelector(astName: string, astType: string, opt?: {
+    forgive?: boolean;
+    warn?: boolean;
+}): void;
+export function matchDirectionPseudoClass(ast: object, node: object): boolean;
+export function matchLanguagePseudoClass(ast: object, node: object): boolean;
+export function matchAttributeSelector(ast: object, node: object): boolean;
+export function matchTypeSelector(ast: object, node: object, opt?: {
+    forgive?: boolean;
+}): boolean;
