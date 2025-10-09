3a0b362f6a98c552caaaaf42f8119685d230c4ba
asamuzaK (Kazz) <kazz@asamuzak.jp>
2025-10-09 20:07:18 +0900

    Update constant.js

--- test/constant.test.js
+++ test/constant.test.js
@@ -12,21 +12,25 @@ import * as constant from '../src/js/constant.js';
 describe('constants', () => {
   const items = Object.entries(constant);
   for (const [key, value] of items) {
-    it('should get string, number or array', () => {
+    it('should get string, number, array or set', () => {
       assert.strictEqual(/^[A-Z][A-Z_\d]+$/.test(key), true, 'key');
-      assert.strictEqual(
-        typeof value === 'string' ||
-          Number.isInteger(value) ||
-          Array.isArray(value),
-        true,
-        'value'
-      );
-      if (Array.isArray(value)) {
-        assert.throws(() => {
-          value[0] = 'foo';
-        });
-        for (const i of value) {
-          assert.strictEqual(typeof i, 'string');
+      if (key.startsWith('KEYS_')) {
+        assert.strictEqual(value instanceof Set, true, 'Set value');
+      } else {
+        assert.strictEqual(
+          typeof value === 'string' ||
+            Number.isInteger(value) ||
+            Array.isArray(value),
+          true,
+          'value'
+        );
+        if (Array.isArray(value)) {
+          assert.throws(() => {
+            value[0] = 'foo';
+          });
+          for (const i of value) {
+            assert.strictEqual(typeof i, 'string');
+          }
         }
       }
     });
