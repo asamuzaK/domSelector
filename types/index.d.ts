303c903ec2741f4fc3c54c5d39ae6f5d0457bdfa
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-04-07 01:15:38 +0900

    v7.0.7

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,14 @@
+export class GenerationalCache {
+    constructor(max: any);
+    max: number;
+    current: Map<any, any>;
+    old: Map<any, any>;
+    get(key: any): any;
+    set(key: any, value: any): void;
+    has(key: any): boolean;
+    delete(key: any): void;
+    clear(): void;
+}
 export class DOMSelector {
     constructor(window: Window, document: Document, opt?: object);
     clear: () => void;
