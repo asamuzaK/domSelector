d4b7e28e6f9e057840139129b90d9a9033fe0032
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-08-29 10:05:34 +0900

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -48,6 +48,8 @@ export default [
         }
       ],
       'prefer-object-has-own': 'error',
+      'unicorn/no-for-each': 'error',
+      'unicorn/no-for-loop': 'error',
       'unicorn/prefer-node-protocol': 'error'
     }
   },
