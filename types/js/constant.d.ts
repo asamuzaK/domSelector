14632aeea12740489846117d200bce8ca3fa598c
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-09-10 21:01:41 +0900

    v9.1.2

--- types/js/constant.d.ts
+++ types/js/constant.d.ts
@@ -87,3 +87,4 @@ export declare const KEYS_LOGICAL: Set<string>;
 export declare const KEYS_PS_CLASS_SUPPORTED: Set<string>;
 export declare const KEYS_PS_CLASS_UNSUPPORTED: Set<string>;
 export declare const KEYS_PS_ELEMENT_UNSUPPORTED: Set<string>;
+export declare const MIME_HTML: string;
