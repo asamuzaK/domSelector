3e9a8ffdc7cae7c21548cca18e798bb4206955f6
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-09-18 23:26:42 +0900

    v9.2.0

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,6 +8,12 @@ export type CheckResult = {
     match: boolean;
     pseudoElement: string | null;
     ast: import('css-tree').CssNode | null;
+    error: DOMException | Error | null;
+};
+export type SelectorSubject = {
+    id: string | null;
+    className: string | null;
+    tag: string | null;
 };
 export type UserOptions = {
     noexcept?: boolean;
@@ -27,22 +33,20 @@ export declare class DOMSelector {
     constructor(window: Window, document: Document, opt?: {
         cacheSize?: number;
         idlUtils?: object;
+        maxLength?: number;
     });
     clear(clearAll?: boolean): void;
-    extractSubjects(selector: string, caseSensitive?: boolean): Array<{
-        id: string | null;
-        className: string | null;
-        tag: string | null;
-    }>;
+    extractSubjects(selector: string, caseSensitive?: boolean): Array<SelectorSubject>;
     supports(selector: string): boolean;
     check(selector: string, node: Element, opt?: {
         requireAst?: boolean;
-    }): CheckResult | null;
+    }): CheckResult;
     matches(selector: string, node: Element, opt?: UserOptions): boolean;
     closest(selector: string, node: Element, opt?: UserOptions): Element | null;
     querySelector(selector: string, node: Document | DocumentFragment | Element, opt?: UserOptions): Element | null;
     querySelectorAll(selector: string, node: Document | DocumentFragment | Element, opt?: UserOptions): Array<Element>;
     private #wrapNode;
+    private #validateSelector;
     private #validateNodeType;
     private #tryNwsapi;
     private #findNodes;
